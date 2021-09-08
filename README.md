ed8c30328645958dfddaa8e530324b52fc780af2
Alan W. Smith <alan@alanwsmith.com>
2021-09-08 17:32:49 -0400

    Put in GitHub example

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Netlify Nextjs NextAuth V3 Test
 
 Like it says on the tin
+
+Setup was done via:
+
+https://next-auth.js.org/v3/getting-started/example
+
+This also requires setting up an OAuth application for
+the providers you want to use:
+
+GitHub: https://github.com/settings/developers
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>Hello, world</div>
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+export default function Page() {
+  const [session, loading] = useSession()
+
+  return (
+    <>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user.name} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </>
+  )
 }
