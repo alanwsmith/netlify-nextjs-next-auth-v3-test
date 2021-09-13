3233d435709051bb3f83b2ffc2f2cad4db763c52
Alan W. Smith <alan@alanwsmith.com>
2021-09-13 00:45:29 -0400

    Progress getting the pages to work

--- components/LayoutWithSessionCheck/LayoutWithSessionCheck.js
+++ components/LayoutWithSessionCheck/LayoutWithSessionCheck.js
@@ -16,7 +16,6 @@ function TheLayout({ securePage, session, children }) {
           <Link href="/">
             <a>Home Page</a>
           </Link>{' '}
-          ~{' '}
           <Link href="/unprotected-page">
             <a>Unprotected Page</a>
           </Link>
@@ -45,28 +44,65 @@ function TheLayout({ securePage, session, children }) {
 // requries login flag, of sorts, or just kick over to
 // the signin pages.
 export default function LayoutWithSessionCheck({ securePage, children }) {
+  console.log('Start render LayoutWithSessionCheck')
+
   const [session, loading] = useSession()
-  const [loadPage, setLoadPage] = useState(false)
+  // const [loadPage, setLoadPage] = useState(false)
   const isUser = !!session?.user
+
+  console.log(`Session 1: ${session}`)
+  console.log(`Loading 1: ${loading}`)
+  // console.log(`loadPage 1: ${loadPage}`)
+  console.log(`isUser 1: ${isUser}`)
+
   useEffect(() => {
+    console.log('Caught Use effect')
+    console.log(`Session 2: ${session}`)
+    console.log(`Loading 2: ${loading}`)
+    // console.log(`loadPage 2: tmp removed`)
+    console.log(`isUser 2: tmp removed`)
+
     if (loading) return
-    if (session !== undefined && loading === false) {
-      setLoadPage(true)
-    }
+
+    // if (session !== undefined && loading === false) {
+    //   console.log(`### setLoadPage = true`)
+    //   setLoadPage(true)
+    // }
+
+    // if (session !== undefined && loading === false) {
+    //   ;<TheLayout securePage={securePage} session={session}>
+    //     {children}
+    //   </TheLayout>
+    //   // setLoadPage(true)
+    // }
+
     // Lock down pages and require user access
-    if (session !== undefined && loading === false && !isUser && securePage) {
+    if (session === null && loading === false && securePage) {
       signIn()
     }
-  }, [loading, session, isUser, securePage])
+  }, [loading, session, securePage])
 
-  if (loadPage) {
-    setLoadPage(false)
+  // Atempting to prevent flash of conent before redirect
+  if (session === null && loading === false && securePage) {
+    return null
+  }
+
+  if (session !== undefined && loading === false) {
+    console.log('Caught loadPage')
+    console.log(`Session 3: ${session}`)
+    console.log(`Loading 3: ${loading}`)
+    // console.log(`loadPage 3: ${loadPage}`)
+    console.log(`isUser 3: ${isUser}`)
+    // setLoadPage(false)
+    // console.log(`### setLoadPage = false`)
     return (
       <TheLayout securePage={securePage} session={session}>
         {children}
       </TheLayout>
     )
   }
+
+  console.log('At end of the funciton')
 
   return null
 }
--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import { useEffect } from 'react'
 export default function App({ Component, pageProps }) {
   // Secure pages by default
   const securePage = Component.secure !== undefined ? Component.secure : true
+  console.log(`Secure Page Initial State: ${securePage}`)
   return (
     <Provider session={pageProps.session}>
       <LayoutWithSessionCheck securePage={securePage}>
