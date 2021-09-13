3dedf9a1d35658750a9b67921dbe57b1d26729ea
Alan W. Smith <alan@alanwsmith.com>
2021-09-12 21:51:52 -0400

    This looks to be working. still going to test a little.

--- components/LayoutWithSessionCheck/LayoutWithSessionCheck.js
+++ components/LayoutWithSessionCheck/LayoutWithSessionCheck.js
@@ -1,18 +1,35 @@
-import { useSession, signIn } from 'next-auth/client'
+import { useSession, signIn, signOut } from 'next-auth/client'
 import Link from 'next/link'
 import { useEffect } from 'react'
 
-function TheLayout({ isUser, children }) {
-  console.log(isUser)
-  return (
-    <>
-      <div>data</div>
-      <div>{isUser}</div>
-      <div>{children}</div>
-    </>
-  )
+function TheLayout({ session, children }) {
+  if (session) {
+    return (
+      <>
+        <div>
+          User is: <button onClick={() => signOut()}>Sign out</button>
+        </div>
+        {children}
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div>
+          User is not logged in:{' '}
+          <button onClick={() => signIn()}>Sign in</button>
+        </div>
+      </>
+    )
+  }
 }
 
+// This is how your checking to see if the user is logged in
+// at the layout level. One improvement would be to setup
+// so that you can flag pages that requrie auth and have them
+// kick over to pages that don't need it that way. A
+// requries login flag, of sorts, or just kick over to
+// the signin pages.
 export default function LayoutWithSessionCheck({ children }) {
   const [session, loading] = useSession()
   useEffect(() => {
@@ -32,42 +49,4 @@ export default function LayoutWithSessionCheck({ children }) {
   }
 
   return null
-
-  return (
-    <SessionCheck>
-      <>
-        <div>
-          Test Links:{' '}
-          <Link href="/">
-            <a>Home Page</a>
-          </Link>{' '}
-          ~{' '}
-          <Link href="/unprotected-page">
-            <a>Unprotected page</a>
-          </Link>{' '}
-          ~{' '}
-          <Link href="/protected-page">
-            <a>Protected Page</a>
-          </Link>
-        </div>
-        {children}
-      </>
-    </SessionCheck>
-  )
-}
-
-function SessionCheck({ children }) {
-  const [session, loading] = useSession()
-  const isUser = !!session?.user
-  useEffect(() => {
-    if (loading) {
-      return null
-    }
-    if (!isUser) signIn()
-  }, [isUser, loading])
-
-  if (loading) {
-    return null
-  }
-  return children
 }
