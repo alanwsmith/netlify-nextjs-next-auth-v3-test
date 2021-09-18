57b4a114906cfa0e6b342d9b6ec307f4f1d6d759
Alan W. Smith <alan@alanwsmith.com>
2021-09-18 15:08:28 -0400

    Removed comments

--- components/LayoutWithSessionCheck/LayoutWithSessionCheck.js
+++ components/LayoutWithSessionCheck/LayoutWithSessionCheck.js
@@ -1,6 +1,6 @@
 import { useSession, signIn, signOut } from 'next-auth/client'
+import { useEffect } from 'react'
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
 
 function TheLayout({ securePage, session, children }) {
   return (
@@ -35,69 +35,14 @@ function TheLayout({ securePage, session, children }) {
       {children}
     </>
   )
-  console.log(session)
-
-  if (session) {
-    return (
-      <>
-        <div>
-          {session && <button onClick={() => signOut()}>Sign out</button>}
-          Current User: {session.user.name}{' '}
-          <button onClick={() => signOut()}>Sign out</button>
-        </div>
-        {children}
-      </>
-    )
-  } else {
-    return (
-      <>
-        {<button onClick={() => signOut()}>Sign out</button>}
-        <div style={{ background: 'blue' }}>
-          <button onClick={() => signIn()}>Sign in</button>
-        </div>
-      </>
-    )
-  }
 }
 
-// This is how your checking to see if the user is logged in
-// at the layout level. One improvement would be to setup
-// so that you can flag pages that requrie auth and have them
-// kick over to pages that don't need it that way. A
-// requries login flag, of sorts, or just kick over to
-// the signin pages.
 export default function LayoutWithSessionCheck({ securePage, children }) {
-  console.log('Start render LayoutWithSessionCheck')
-
   const [session, loading] = useSession()
-  // const [loadPage, setLoadPage] = useState(false)
   const isUser = !!session?.user
 
-  console.log(`Session 1: ${session}`)
-  console.log(`Loading 1: ${loading}`)
-  // console.log(`loadPage 1: ${loadPage}`)
-  console.log(`isUser 1: ${isUser}`)
-
   useEffect(() => {
-    console.log('Caught Use effect')
-    console.log(`Session 2: ${session}`)
-    console.log(`Loading 2: ${loading}`)
-    // console.log(`loadPage 2: tmp removed`)
-    console.log(`isUser 2: tmp removed`)
-
     if (loading) return
-
-    // if (session !== undefined && loading === false) {
-    //   console.log(`### setLoadPage = true`)
-    //   setLoadPage(true)
-    // }
-
-    // if (session !== undefined && loading === false) {
-    //   ;<TheLayout securePage={securePage} session={session}>
-    //     {children}
-    //   </TheLayout>
-    //   // setLoadPage(true)
-    // }
 
     // Lock down pages and require user access
     if (session === null && loading === false && securePage) {
@@ -105,27 +50,18 @@ export default function LayoutWithSessionCheck({ securePage, children }) {
     }
   }, [loading, session, securePage])
 
-  // Atempting to prevent flash of conent before redirect
+  // Remove flash of unauthenticated content
   if (session === null && loading === false && securePage) {
     return null
   }
 
   if (session !== undefined && loading === false) {
-    console.log('Caught loadPage')
-    console.log(`Session 3: ${session}`)
-    console.log(`Loading 3: ${loading}`)
-    // console.log(`loadPage 3: ${loadPage}`)
-    console.log(`isUser 3: ${isUser}`)
-    // setLoadPage(false)
-    // console.log(`### setLoadPage = false`)
     return (
       <TheLayout securePage={securePage} session={session}>
         {children}
       </TheLayout>
     )
   }
-
-  console.log('At end of the funciton')
 
   return null
 }
